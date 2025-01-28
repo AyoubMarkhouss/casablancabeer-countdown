c4c6039e8f6e9a610ba9a0ffb3c58a2866cf63c1
AyoubMarkhouss <ayoub.markhouss@gmail.com>
2025-01-28 10:19:22 +0100

    count

---
+++ _next/static/Xknot175FuORZUgMT8K1G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/cb355538-d051fa04070a41be.js","static/chunks/380-85b41c30643ab2e3.js","static/chunks/pages/index-66c920790eff26cc.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
