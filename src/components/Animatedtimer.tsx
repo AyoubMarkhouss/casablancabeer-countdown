98ff3032e9bb847825beecd9a21cb71f13cdf976
AyoubMarkhouss <ayoub.markhouss@gmail.com>
2025-01-28 11:16:55 +0100

    age verification

--- src/components/Animatedtimer.tsx
+++ src/components/Animatedtimer.tsx
@@ -4,6 +4,18 @@ import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import { FaFacebook, FaInstagram } from "react-icons/fa";
 import Link from "next/link";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
 export default function AnimatedTimer() {
   const [timerDays, setTimerDays] = useState("00");
   const [timerHours, setTimerHours] = useState("00");
@@ -35,14 +47,38 @@ export default function AnimatedTimer() {
       }
     }, 1000);
   };
+  // const startTimer = () => {
+  //   const countdownDate = new Date("January 28, 2025 10:38:00").getTime();
+  //   interval.current = setInterval(() => {
+  //     const now = new Date().getTime();
+  //     const distance = countdownDate - now;
 
+  //     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //     const hours = Math.floor(
+  //       (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  //     );
+  //     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  //     if (distance < 0) {
+  //       if (interval.current) clearInterval(interval.current);
+  //       // Redirect to the desired URL when the countdown ends
+  //       window.location.href = "https://www.google.com"; // Replace with your target URL
+  //     } else {
+  //       setTimerDays(String(days).padStart(2, "0"));
+  //       setTimerHours(String(hours).padStart(2, "0"));
+  //       setTimerMinutes(String(minutes).padStart(2, "0"));
+  //       setTimerSeconds(String(seconds).padStart(2, "0"));
+  //     }
+  //   }, 1000);
+  // };
   useEffect(() => {
     startTimer();
     return () => {
       if (interval.current) clearInterval(interval.current);
     };
   }, []);
-
+  const [open, setOpen] = useState(true);
   return (
     <div className="h-screen relative">
       <Image
@@ -71,6 +107,37 @@ export default function AnimatedTimer() {
           <h1 className="font-bold z-50 pb-4 text-4xl md:text-5xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
             READY TO REINVENT YOUR SPIRIT ?
           </h1>
+          <div className="z-50">
+            <AlertDialog open={open} onOpenChange={setOpen}>
+              <AlertDialogTrigger className="hidden" />
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle className="text-lg">
+                    VÉRIFICATION DE L'ÂGE
+                  </AlertDialogTitle>
+                  <AlertDialogDescription className="modalfont text-md">
+                    VOUS DEVEZ AVOIR AU MOINS 18 ANS POUR ACCÉDER À CE CONTENU.
+                    VEUILLEZ CONFIRMER VOTRE ÂGE.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel
+                    onClick={() =>
+                      (window.location.href = "https://www.google.com")
+                    }
+                  >
+                    J’ai moins de 18 ans
+                  </AlertDialogCancel>
+                  <AlertDialogAction
+                    className="bg-red-800"
+                    onClick={() => setOpen(false)}
+                  >
+                    J’ai 18 ans ou plus
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
         </div>
         <div className="w-full max-w-5xl text-white z-50 mx-auto flex items-center ">
           <CountdownItem num={timerDays} text="Jours" />
@@ -79,13 +146,13 @@ export default function AnimatedTimer() {
           <CountdownItem num={timerSeconds} text="Secondes" />
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 pt-32 xl:pt-10 2xl:pt-32">
-        <div className="hidden  pt-0 lg:pt-14 2xl:pt-32  z-10 md:flex justify-center items-center text-sm text-white">
+      <div className="grid grid-cols-1 md:grid-cols-3 pt-48 xl:pt-10 2xl:pt-32">
+        <div className="hidden  pt-0 lg:pt-14 2xl:pt-32  z-10 md:flex justify-center items-center text-xs text-white">
           CASABLANCA BEER © TOUS DROITS RÉSERVÉS
         </div>
         <div></div>
 
-        <div className=" pt-0 lg:pt-14 2xl:pt-32 z-10 flex gap-x-2 justify-center items-center  text-white">
+        <div className=" pt-0 lg:pt-14 2xl:pt-32 z-10 flex gap-x-2 text-sm justify-center items-center  text-white">
           SUIVEZ-NOUS SUR
           <div className="flex items-center gap-x-2">
             <Link
@@ -109,7 +176,7 @@ export default function AnimatedTimer() {
             </Link>
           </div>
         </div>
-        <div className="font-semibold md:hidden z-10 flex justify-center items-center text-sm text-white">
+        <div className="font-semibold md:hidden z-10 flex justify-center items-center text-[10px] text-white">
           CASABLANCA BEER © TOUS DROITS RÉSERVÉS
         </div>
       </div>
